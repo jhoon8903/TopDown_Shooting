dd4d29bce51c651717db65337d016149820b85ec
Daniel Lee <jhoon8903@gmail.com>
2023-12-21 16:20:49 +0900

    commit

---
+++ Assets/Scripts/Controller/TopDownEnemyRangeController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownEnemyRangeController : TopDownEnemyController
+{
+    [SerializeField] private float followRange = 15f;
+    [SerializeField] private float shootRange = 10f;
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+        float distance = DistanceToTarget();
+        Vector2 direction = DirectionToTarget();
+
+        IsAttack = false;
+
+        if (distance <= followRange)
+        {
+            if (distance <= shootRange)
+            {
+                int layerMaskTarget = CharacterStatsHandler.CurrentStates.attackSo.target;
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, 11f,
+                    (1 << LayerMask.NameToLayer("Level")) | layerMaskTarget);
+
+                if (hit.collider != null && layerMaskTarget == (layerMaskTarget | (1<<hit.collider.gameObject.layer)))
+                {
+                    CallLookEvent(direction);
+                    CallMoveEvent(Vector2.zero);
+                    IsAttack = true;
+                }
+            }
+            else
+            {
+                CallMoveEvent(direction);
+            }
+        }
+        else
+        {
+            CallMoveEvent(direction);
+        }
+    }
+
+}
